391abe81df1817033d22cb6adbb8c917d7223811
prudhvi.rambha@lseg.com <prudhvi.rambha@lseg.com>
2023-08-01 05:25:25 +0530

    Fist commit

---
+++ udemy-spring/src/main/java/com/udemy/spring/basics/springin5steps/QuicksortAlgo.java
@@ -0,0 +1,13 @@
+package com.udemy.spring.basics.springin5steps;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Component
+@Primary
+public class QuicksortAlgo implements SortAlgorithm {
+    public int[] sort(int[] numbers){
+        //Logic for Quick Sort
+        return numbers;
+    }
+}
